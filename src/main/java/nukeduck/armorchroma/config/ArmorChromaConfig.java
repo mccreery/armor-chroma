f46152f641fb075d8e0518673d8b4acc3a1e177e
Sam McCreery <4602020+mccreery@users.noreply.github.com>
2018-10-05 09:31:14 +0100

    Add localization to config options

--- src/main/java/nukeduck/armorchroma/config/ArmorChromaConfig.java
+++ src/main/java/nukeduck/armorchroma/config/ArmorChromaConfig.java
@@ -3,6 +3,7 @@ package nukeduck.armorchroma.config;
 import net.minecraftforge.common.config.Config;
 import net.minecraftforge.common.config.ConfigManager;
 import net.minecraftforge.common.config.Config.Comment;
+import net.minecraftforge.common.config.Config.LangKey;
 import net.minecraftforge.fml.client.event.ConfigChangedEvent;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
@@ -12,14 +13,16 @@ import nukeduck.armorchroma.ArmorChroma;
 @EventBusSubscriber
 public class ArmorChromaConfig {
 	@Comment("Display enchanted glint")
+	@LangKey("armorchroma.config.renderGlint")
 	public static boolean renderGlint = true;
 
 	@Comment("Compress the bar into different colored borders when your armor exceeds 20")
+	@LangKey("armorchroma.config.compressBar")
 	public static boolean compressBar;
 
 	@SubscribeEvent
 	public static void onConfigChanged(ConfigChangedEvent.OnConfigChangedEvent event) {
-		if (event.getModID().equals(ArmorChroma.MODID)) {
+		if(event.getModID().equals(ArmorChroma.MODID)) {
 			ConfigManager.sync(ArmorChroma.MODID, Config.Type.INSTANCE);
 		}
 	}
