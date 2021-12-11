53873a773e9f85595809d04684e44ba5c5711a7f
A5b84 <42934711+A5b84@users.noreply.github.com>
2021-12-11 14:48:57 +0100

    Make compat mixin injection conditional (removes a warning)

---
+++ src/main/java/nukeduck/armorchroma/mixin/MixinPlugin.java
@@ -0,0 +1,29 @@
+package nukeduck.armorchroma.mixin;
+
+import net.fabricmc.loader.api.FabricLoader;
+import org.objectweb.asm.tree.ClassNode;
+import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
+import org.spongepowered.asm.mixin.extensibility.IMixinInfo;
+
+import java.util.List;
+import java.util.Set;
+
+public class MixinPlugin implements IMixinConfigPlugin {
+
+    @Override
+    public boolean shouldApplyMixin(String targetClassName, String mixinClassName) {
+        if (mixinClassName.startsWith("extendedarmorbars.", "nukeduck.armorchroma.mixin.compat.".length())) {
+            return FabricLoader.getInstance().isModLoaded("extended_armor_bars");
+        } else {
+            return true;
+        }
+    }
+
+    @Override public void onLoad(String mixinPackage) {}
+    @Override public String getRefMapperConfig() { return null; }
+    @Override public void acceptTargets(Set<String> myTargets, Set<String> otherTargets) {}
+    @Override public List<String> getMixins() { return null; }
+    @Override public void preApply(String targetClassName, ClassNode targetClass, String mixinClassName, IMixinInfo mixinInfo) {}
+    @Override public void postApply(String targetClassName, ClassNode targetClass, String mixinClassName, IMixinInfo mixinInfo) {}
+
+}
